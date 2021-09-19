020c87b73dd60d838b84953e127a369aede01af9
Ainul130496 <90658052+Ainul130496@users.noreply.github.com>
2021-09-19 18:57:06 +0700

    Add files via upload
    Reupload Subscribe dulu slur

---
+++ Run.sh
@@ -0,0 +1,50 @@
+clear
+cyan='\033[0;36m'
+purple='\033[0;35m'
+brown='\033[0;33m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightblue='\033[1;34m'
+lightgreen='\033[1;32m'
+lightcyan='\033[1;36m'
+lightred='\033[1;31m'
+lightpurple='\033[1;35m'
+yellow='\033[1;33m'
+white='\033[1;37m'
+sleep 1
+echo
+figlet "Speed"  | lolcat
+toilet -f standard "Wifi" -F gay
+date | lolcat
+echo ______________________________________√
+echo $lightpurple "WELCOME TO CHANNEL Qiin࿐ Creator" | lolcat
+echo ________________________________¶
+sleep 2
+echo $yellow"Pilih tools nya :"
+echo
+echo $lightgray"1.) Stabilkan Jaringan"
+echo
+echo $darkgray"2.)keluar !!!"
+echo
+echo $lightred
+read -p "Pilih menu : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Speed" | lolcat
+echo $white"Subcribe My Channel : Qiin࿐ Creator"
+echo ®____________Running Speed_____________ √
+sleep 2
+ping -s10000 1.1.1.1
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $yellow
+figlet "Qiin   Creator"
+echo "Jangan Lupa SUBCRIBE !!!"
+echo
+sleep 3
+exit
+fi
+
